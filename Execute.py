a9410c6d31369f6a6725d1cc30d7d327121d8a98
Aurora <treibley95@gmail.com>
2025-09-25 18:17:35 -0600

    added the .gitignore file and added comments to all of the code so it can be readable for the new collaborator and anyone else trying to read the code because even i struggle

--- Execute.py
+++ Execute.py
@@ -10,6 +10,7 @@ def main(screen_size="800x600"):
     #construct and set params for login window
     root = tk.Tk()
     root.title("Login")
+    root.geometry("400x300")
 
 
     #functions used by the window
@@ -19,19 +20,28 @@ def main(screen_size="800x600"):
         #if there is anything in the user_entry feild
         if username:
             #add the username to the file
-            j.write_file_dict("all_users.json", username.lower())
+            j.write_file_dict("ignore/all_users.json", username.lower())
             lbl_login.configure(text="Signup complete, please login")
         #clear the user_entry feild
         user_entry.delete(0, tk.END)
 
     def login_check():
-        all_users = j.read_file('all_users.json')
+        #loads the dict of all users
+        all_users = j.read_file('ignore/all_users.json')
+        #receives the username from the entry box
         username = user_entry.get()
+        #if the username is in the keys
         if username.lower() in all_users.keys():
+            #tell the user the login was successful, i honestly dont think anyone will see this
+            #i can maybe put like a wait command in here to make it feel like the program is loading but thats just to make the user happen
             lbl_login.configure(text=f"Login Successful for {username.lower()}")
+            #closes the login window
             root.destroy()
+            #opens the main menu on the other file, this file should end and not be used after this
             Menu.main_menu(all_users[username.lower()])
+        #if the username was not in the
         else:
+            #tell the user that their username was invalid and prompt them to try again
             lbl_login.configure(text=f"Login Failed for {username.lower()}, please try again")
 
 
